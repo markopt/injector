fe2f32fc48c8970d7d5440342069110480a97eee
Mark Huntley <markhuntley9@gmail.com>
2022-06-23 13:25:35 -0700

    adding inject.js file

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
-let script = "<script src="https://cdn.split.io/sdk/split-browser-0.6.0.full.min.js"></script>";
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ script });
-  console.log('Injecting Split Browser SDK');
+// listen for our browerAction to be clicked
+chrome.browserAction.onClicked.addListener(function (tab) {
+	// for the current tab, execute the Split's Browser SDK script
+	chrome.tabs.executeScript(tab.ib, {
+		file: 'inject.js'
+	});
 });
